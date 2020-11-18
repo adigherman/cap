a0c10d3c584845991d071e95a7d1c6f4426babc1
Adi G <adig@jhu.edu>
2020-11-18 15:07:12 -0500

    Version id 9.1 stable [ci skip]

--- tic.R
+++ tic.R
@@ -1,2 +1,2 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
+#  installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(args=c("--as-cran","--install-args=--build"))
